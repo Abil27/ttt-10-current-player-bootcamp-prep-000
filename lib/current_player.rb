7a78d2fee99e840ce69e24be07206a6bff10053e
Houssein Abil <milameet@gmail.com>
2018-09-07 22:57:25 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+  board.each do |value|
+    if (value == "X") || (value == "O")
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  turn = nil
+  if turn_count % 2 = 0
+    turn = "X"
+  else
+    turn = "O"
+  end
+
+end
